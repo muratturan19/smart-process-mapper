e5fcf5f9ec69a737a8b7edcdbd45aa7ae9c6dd4b
muratturan19 <muratturan19@gmail.com>
2025-05-25 17:47:13 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
 pip install -r requirements.txt
-
-# Install the Turkish spaCy model
 pip install https://huggingface.co/turkish-nlp-suite/tr_core_news_md/resolve/main/tr_core_news_md-any-py3-none-any.whl
+python -m spacy link tr_core_news_md tr_core_news_md
